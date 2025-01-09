d39abd31154b186eb0bb190c57642e39cf93de23
Alex <sacelani@gmail.com>
2025-01-08 23:57:18 -0500

    pi-id bones and a quick todo list

---
+++ pi/pi_id.sh
@@ -0,0 +1,32 @@
+# ==============================================================================
+# Auth: dodd
+# File: pi_id.sh
+# Revn: 01-04-2025  0.1
+# Func: ask user questions about new device
+#
+# TODO: bro figure out the ps1 thing
+#       also like can i change the fucking hostname?
+# ==============================================================================
+# CHANGE LOG
+# ------------------------------------------------------------------------------
+# 01-04-2025: init
+#
+# ==============================================================================
+
+
+## ask for host name???
+# get user input
+# hostnamectl set-hostname $hstnm
+
+# ask for username
+# change groupname from default to user input
+# get user input
+# 
+
+# ask for new password?
+# get user input
+# echo '$usr:$pwd' | sudo chpasswd
+
+# ask about PS1????
+# dump PS1 case/esac into .bash_profile
+
